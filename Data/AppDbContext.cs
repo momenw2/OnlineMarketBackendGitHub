4a0343d40d8941e4f2749ea1b1211c0afc8c92b7
Hussein Abdelmoez <hussein.abdelmoez@csdevents.com>
2025-01-14 12:41:10 +0700

    Added AppDbContext with DbSet<User> to manage database access.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineMarketApi.Models;
+
+namespace OnlineMarketApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
