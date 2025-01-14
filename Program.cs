54a9e905954397cfb1cddf16d2a842a163daaf23
Hussein Abdelmoez <hussein.abdelmoez@csdevents.com>
2025-01-14 11:32:30 +0700

    Configure DbContext with PostgreSQL

--- Program.cs
+++ Program.cs
@@ -1,4 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using PostgreSQL.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add DbContext for PostgreSQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("WebApiDatabase")));
+
 var app = builder.Build();
 
 app.Run();
+
+// AppDbContext.cs
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace PostgreSQL.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+    }
+}
